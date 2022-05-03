b2adf16ad3812e7fbe2c0ac877dfff05feed1f99
itsvitoracacio <vitor.acacio@pm.me>
2022-05-03 17:08:29 -0300

    Page transition animation home > single project page is partially done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const cursor = document.querySelector('.inner-cursor')
+/* const cursor = document.querySelector('.inner-cursor')
 const cursorWrapper = document.querySelector('.outer-cursor')
 document.addEventListener('mousemove', e => {
 	cursor.style.top = `${e.pageY - 5}px`
@@ -13,6 +13,13 @@ document.addEventListener('mousedown', _ => {
 })
 document.addEventListener('mouseup', _ => {
 	cursorWrapper.classList.remove('cursorClick')
-})
+}) */
 // need to make the outer circle grow back smoothly
 // need to add a delay to the outer circle when following the inner circle
+const swup = new Swup()
+
+document.querySelector('.menu-toggle').addEventListener('click', _ => {
+	document
+		.querySelector('.main-proj-thumb-wrapper')
+		.classList.add('proj-thumb-anim')
+})
