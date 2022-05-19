fdf550f9cbc4be749912a297794386fafac3cab2
itsvitoracacio <vitor.acacio@pm.me>
2022-05-19 17:40:04 -0300

    Implemented infinite scroll between projects for the project details part of the homepage

--- homepage.js
+++ homepage.js
@@ -1,10 +1,12 @@
 const swup = new Swup()
 
-document.body.addEventListener('mousewheel', function () {
-	const projDetails = document.querySelector('.project-details')
-	const projName = document.querySelector('.cur-proj-name')
-	const projCategory = document.querySelector('.cur-proj-cat')
+const projDetails = document.querySelector('.project-details')
+let projName = document.querySelector('.cur-proj-name')
+let projCategory = document.querySelector('.cur-proj-cat')
+let hiddenNameArr = [...document.querySelectorAll('.hidden-proj-name')]
+let hiddenCatArr = [...document.querySelectorAll('.hidden-proj-cat')]
 
+document.body.addEventListener('click', function () {
 	const block1 = document.createElement('div')
 	const block2 = document.createElement('div')
 	block1.classList.add('proj-deets-block', 'block1')
@@ -15,17 +17,45 @@ document.body.addEventListener('mousewheel', function () {
 	projName.classList.add('hideDownText')
 	projCategory.classList.add('hideUpText')
 
-	const hiddenName = document.querySelector('.hidden-proj-name')
-	const hiddenCat = document.querySelector('.hidden-proj-cat')
-	hiddenName.classList.add('showUpText')
-	hiddenCat.classList.add('showDownText')
+	hiddenNameArr[0].classList.add('showUpText')
+	hiddenCatArr[0].classList.add('showDownText')
 
-	// setTimeout(() => {
-	// 	projName.classList.remove('slideDownText')
-	// 	projCategory.classList.remove('slideUpText')
-	// 	projDeetsBlock1.remove()
-	// 	projDeetsBlock2.remove()
-	// }, 1500)
+	console.log(projName)
+	console.log(
+		`
+		0: ${hiddenNameArr[0].innerHTML},
+		1: ${hiddenNameArr[1].innerHTML},
+		2: ${hiddenNameArr[2].innerHTML}`
+	)
+
+	setTimeout(() => {
+		projName.classList.remove('hideDownText')
+		projCategory.classList.remove('hideUpText')
+		hiddenNameArr[0].classList.remove('showUpText')
+		hiddenCatArr[0].classList.remove('showDownText')
+		document.querySelector('.block1').remove()
+		document.querySelector('.block2').remove()
+
+		projName.classList.add('hidden-proj-name')
+		projCategory.classList.add('hidden-proj-cat')
+		projName.classList.remove('cur-proj-name')
+		projCategory.classList.remove('cur-proj-cat')
+
+		hiddenNameArr[0].classList.add('cur-proj-name')
+		hiddenCatArr[0].classList.add('cur-proj-cat')
+		hiddenNameArr[0].classList.remove('hidden-proj-name')
+		hiddenCatArr[0].classList.remove('hidden-proj-cat')
+
+		hiddenNameArr.shift()
+		hiddenNameArr.push(projName)
+		hiddenCatArr.shift()
+		hiddenCatArr.push(projCategory)
+
+		projName = document.querySelector('.cur-proj-name')
+		projCategory = document.querySelector('.cur-proj-cat')
+		// console.log(projName)
+		// console.log(hiddenNameArr[1])
+	}, 1500)
 })
 
 document.body.addEventListener('mousewheel', function () {
