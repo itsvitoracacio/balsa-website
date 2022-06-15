841054edeeecf0c2d2612333661b46d2cba8b8e2
itsvitoracacio <vitor.acacio@pm.me>
2022-06-15 14:11:10 -0300

    Added the custom mouse and show project name on hover on the homepage. Changed image on contact page.

---
+++ components/cursor/cursor.js
@@ -0,0 +1,42 @@
+// Styling the custom cursor
+const cursor = document.querySelector('.inner-cursor')
+const cursorWrapper = document.querySelector('.outer-cursor')
+
+// Inner cursor
+window.addEventListener('mousemove', e => {
+	cursor.style.top = `${e.pageY - 5}px`
+	cursor.style.left = `${e.pageX - 6}px`
+})
+
+// Outer cursor
+// window.addEventListener('mousemove', e => {
+// 	cursorWrapper.style.top = `${e.clientY - 13}px`
+// 	cursorWrapper.style.left = `${e.clientX - 14}px`
+// })
+
+// document.addEventListener('mousedown', _ => {
+// 	cursorWrapper.classList.add('cursorClick')
+// })
+// document.addEventListener('mouseup', _ => {
+// 	cursorWrapper.classList.remove('cursorClick')
+// })
+// need to make the outer circle grow back smoothly
+// need to add a delay to the outer circle when following the inner circle
+
+const projName = document.createElement('span')
+
+window.addEventListener('mousemove', e => {
+	const listItem = e.target.parentElement.parentElement
+
+	if (!listItem.dataset.projName) projName.style.opacity = 0
+	else {
+		projName.innerText = listItem.dataset.projName
+		projName.style.position = 'absolute'
+		// projName.style.fontWeight = '700'
+		projName.style.top = `${e.clientY + 15 + window.scrollY}px`
+		projName.style.left = `${e.clientX}px`
+		projName.style.opacity = 1
+
+		listItem.appendChild(projName)
+	}
+})
--- components/menu/menu.js
+++ components/menu/menu.js
@@ -16,23 +16,3 @@ function changeHeaderZIndex() {
 	if (menuCheckbox.checked) setTimeout(() => (header.style.zIndex = '0'), 750)
 	else header.style.zIndex = '10'
 }
-
-// Styling the custom cursor
-/* const cursor = document.querySelector('.inner-cursor')
-const cursorWrapper = document.querySelector('.outer-cursor')
-document.addEventListener('mousemove', e => {
-	cursor.style.top = `${e.pageY - 5}px`
-	cursor.style.left = `${e.pageX - 3}px`
-})
-document.addEventListener('mousemove', e => {
-	cursorWrapper.style.top = `${e.clientY - 16}px`
-	cursorWrapper.style.left = `${e.clientX - 13.5}px`
-})
-document.addEventListener('mousedown', _ => {
-	cursorWrapper.classList.add('cursorClick')
-})
-document.addEventListener('mouseup', _ => {
-	cursorWrapper.classList.remove('cursorClick')
-}) */
-// need to make the outer circle grow back smoothly
-// need to add a delay to the outer circle when following the inner circle
